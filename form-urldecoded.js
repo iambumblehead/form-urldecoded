8c627fbdbb5abb90f07cec59c6153ee513ce7445
chris <chris@bumblehead.com>
2019-08-22 03:27:00 -0700

    added tests, use more re

--- form-urldecoded.js
+++ form-urldecoded.js
@@ -2,89 +2,61 @@
 // Timestamp: 2018.08.15-17:52:43 (last modified)
 // Author(s): frabarz <francisco@datawheel.us>
 
-/**
-* @param {string} uri The query string to parse. Can be a simple urlencoded string or a full URL.
-* @param {Options} opts Additional options
-* @returns {any}
-*/
-const formurldecoded = (module.exports = (data, opts = {}) => {
-  const ignoreNull = Boolean(opts.ignorenull);
-  const skipIndex = Boolean(opts.skipIndex);
-
-  const assignValue = (param, value, target) => {
-    const [key, remnant] = parseKey(param);
-    if (remnant !== undefined) {
-      const targetObj = target[key] || defaultTarget(remnant);
-      assignValue(remnant, value, targetObj);
-      target[key] = targetObj;
-    }
-    else if (!Array.isArray(target) || !skipIndex || isNaN(+key) || key !== "") {
-      target[key] = value;
-    }
-    else {
-      target.push(value);
-    }
-    return target;
-  };
-
-  const defaultTarget = key => {
-    const targetKey = key.split("[", 1).shift();
-    return targetKey === "" || !isNaN(+targetKey) ? [] : {};
-  };
-
-  const parseKey = key => {
-    const openIndex = key.indexOf("[");
-    return openIndex > -1
-      ? [key.slice(0, openIndex), key.slice(openIndex + 1).replace("]", "")]
-      : [key];
-  };
-
-
-  const rmHash = str => String(str).replace(/#.*$/, '');
-
-  const decode = str => decodeURIComponent(String(str)).replace(/\+/g, ' ');
-
-  // remove question mark char not nested in querystring
-  const rmLead = str => str.replace(/[^=]*\?([\s\S]*)/, (m, s) => s);
-
-  // split string w/ '&' not found in nested quotation
-  const split = ( str, parts = [] ) => {
-    if ( !str )
-      return parts;
-
-    var match = str.match(/^([^"&]*)&([\s\S]*)/);
-
-    if ( match )
-      return split( match[2], parts.concat(match[1]) );
-    
-    return split( null, parts.concat( str ) );
-  };
-
-  const splitKeyVal = str => {
-    var match = str.match(/^([^"=]*)=([\s\S]*)/);
-
-    return match ? match.slice(1) : [str];
-  };
+const formurldecoded = module.exports = data => {
+  const keyRemnantRe = /([^\[]*)\[([^"\]]*)?\]([\s\S]*)?/,
+        headQueryRe = /[^=]*\?([\s\S]*)/,
+        restQueryRe = /^([^"&]*)&([\s\S]*)/,
+        keyValRe = /^([^"=]*)=([\s\S]*)/,
+        boolRe = /^(true|false)$/,
+        hashRe = /#.*$/,
+        intRe = /^\d*$/,
   
-  const parseValue = value =>
-    !isNaN(+value)
-      ? +value
-      : /^(true|false)$/.test(value)
-        ? value === "true"
-        : value === "null" || !value ? null : value;
+    rmHash = str => str.replace(hashRe, ''),
+        
+    rmLead = str => str.replace(headQueryRe, (m, s) => s),  
 
-  data = typeof data === 'string'
-    ? decode( rmHash( rmLead( data ) ) ) : '';
+    decode = str => decodeURIComponent(str).replace(/\+/g, ' '),
+
+    splitKeyVal = (str, match = str.match(keyValRe)) =>
+        match ? match.slice(1) : [str],
+        
+    assignValue = (ns, [key, val, subkey], match = key.match(keyRemnantRe)) => {
+      if (match) {
+        [, key, subkey] = match;
+        
+        subkey = intRe.test(subkey) ? parseInt(subkey, 10) : subkey;
+        ns[key] = ns[key] || (/undefined|number/.test(typeof subkey) ? [] : {});
+        subkey = /string|number/.test(typeof subkey) ? subkey : ns[key].length;
+        
+        if (match[3]) {
+          ns[key] = assignValue(ns[key], [subkey + match[3], val]);
+        } else if (val) {
+          ns[key][subkey] = parseValue(val);
+        }
+      } else if (val) {
+        ns[key] = parseValue(val);
+      }
+
+      return ns;
+  },
+
+  split = (str, parts = [], match = str.match(restQueryRe)) => {
+    if (match)
+      parts = split(match[2], parts.concat(match[1]));
+    else if (str)
+      parts = parts.concat(str);
+
+    return parts;
+  },
   
-  return split( data ).reduce((uriVals, param) => {
-    const [key, val] = splitKeyVal( param );
-    const value = parseValue(val);
-    return value != null || !ignoreNull ? assignValue(key, value, uriVals) : uriVals;
-  }, {});
-});
+  parseValue = value => !isNaN(+value)
+    ? +value
+    : boolRe.test(value)
+      ? value === "true"
+      : value === "null" || !value ? null : value;
 
-/**
-* @typedef Options
-* @property {boolean} [ignorenull] Determines if parameters without value should be included in the parsed object. If so, their value will be null.
-* @property {boolean} [skipIndex] Determines if bracketed numbers should be used to set the order of the values in an array.
-*/
+  return typeof data === 'string'
+    ? split(decode(rmHash(rmLead(data))))
+       .reduce((o, qs) => assignValue(o, splitKeyVal(qs)), {})
+    : {};
+};
